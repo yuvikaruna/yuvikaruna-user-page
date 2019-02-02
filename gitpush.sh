cd0ad9f3c7529a648ecf324d498d9b7d314693e9
Yuvaraj-K <yuvikaruna@gmail.com>
2019-02-02 16:37:28 +0530

    home page added

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,14 @@
 git add .
 
+git status
+read
+
 echo 'Enter the commit message:'
 read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
-read branch
 
-git push origin $branch
+git push 
 
 read
